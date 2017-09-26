49b805434d2f7bfa732fa4348fbebbb2075b3842
Marek Zdybel <marek@motta.com.pl>
2017-09-26 22:01:07 +0200

    Szkic programu

---
+++ ZD_01/Wisielec.py
@@ -0,0 +1,15 @@
+# Lista słów
+# Wygenerowanie losowego słowa
+# przysłonięcie listy słów znakami * np: "kot" ***
+# start gry
+#
+#     Wyświetlenie przesłoniętego wyrazu
+#         prośba do użytkownika o podanie literki
+#             sprawdzenie czy literka występuje w wyrazie
+#             jeśli występuje, odsłonięcie literki,
+#             jeśli nie wyświetlenie informacji o nieudanej próbie
+#             powiększenie licznika kroków
+#             powtarzanie kroków do chwili odsłonięcia wszystkich liter,
+#             zużycia wszystkich liter lub wpisania słowa exit które zamyka program
+#
+#     wyświetlenie statystyki gry i wyjście z programu
