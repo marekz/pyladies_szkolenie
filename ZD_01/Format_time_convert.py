060b86bc6a594cad6dfef06cdaf734fce4f7dddb
Marek Zdybel <marek@motta.com.pl>
2017-09-27 00:33:50 +0200

    Do zrobienia

---
+++ ZD_01/Format_time_convert.py
@@ -0,0 +1,8 @@
+# Napisz funkcję, która zamieni czas (HH:MM:SS) na
+# sekundy, minuty, godziny.
+# Przykład:
+# Input : get_time(’2:00:00’, ’s’)
+# Output : 7200
+# Input : get_time(’12:00:00’, ’m’)
+# Output : 720
+# Input : get_time
---
+++ ZD_01/Testy_liczb.py
@@ -0,0 +1,9 @@
+# Napisz funkcję która sprawdzi czy dana liczba jest liczbą brzydką.
+# Liczby brzydkie, to takie które są podzielen przez 2, 3 lub 5
+# Liczba 1 jest również liczbą brzydką
+#
+# Przykłąd:
+# input: uglu(12)
+# output: True
+# input: uglu(11)
+# output: False
