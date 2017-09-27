07f4ce61829400ce43570350e431e32425c8c8d8
Marek Zdybel <marek.zdybel@sbg.com.pl>
2017-09-27 16:03:05 +0200

    Brzydkie liczby :)

--- ZD_01/Testy_liczb.py
+++ ZD_01/Testy_liczb.py
@@ -3,7 +3,35 @@
 # Liczba 1 jest również liczbą brzydką
 #
 # Przykłąd:
-# input: uglu(12)
+# input: ugly(12)
 # output: True
-# input: uglu(11)
+# input: ugly(11)
 # output: False
+
+def sprawdzLiczbe(liczba):
+    status = False
+    if liczba%2==0 or liczba%3==0 or liczba%5==0 or liczba==1:
+        status = True
+    return status
+
+def wyswietlStatusLiczby(statusLiczby):
+    if statusLiczby==True:
+        infoString = "Brzydka liczba :)"
+    else:
+        infoString = "Ładna liczba :)"
+
+    return infoString
+
+graRazJeszcze = True
+
+while graRazJeszcze:
+
+    liczba = int(input("Podaj liczbę do sprawdzenia: "))
+    statusLiczby = sprawdzLiczbe(liczba)
+
+    print(wyswietlStatusLiczby(statusLiczby))
+
+    if "N" == input("Czy chcesz raz jeszcze T/N "):
+        graRazJeszcze = False
+
+# TODO: dopisać wyjątki w przypadku wprowadzenie błędnych danych
