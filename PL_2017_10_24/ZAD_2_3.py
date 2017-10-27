3fee036c9a23585ffef8009a32aaaffe1ab3e674
Marek Zdybel <marek.zdybel@sbg.com.pl>
2017-10-27 09:09:48 +0200

    Operacje na plikach - zadanie 3 - inicjacja

---
+++ PL_2017_10_24/ZAD_2_3.py
@@ -0,0 +1,10 @@
+# 2.3*Zapisz średni wzrost postaci dla każdego z kolorów w osobnych linijkach w formie:
+# Średni wzrost osób z kolorem oczu blue wynosi 123,12 cm.
+
+#Wczytanie pliku
+
+#iteracja po pliku
+
+#   tworzenie wielowymiarowej tablicy z element['kolor oczu']['wzrosty osob']
+
+#Operacja obliczenia średniej dla każdego koloru oczu
