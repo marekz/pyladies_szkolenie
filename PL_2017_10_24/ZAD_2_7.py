6a7131ae394d153a0d26cf2baa45c176d912d501
Marek Zdybel <marek.zdybel@sbg.com.pl>
2017-11-06 20:33:29 +0100

    Zadanie z 3 sesji - Sortowanie statkow

---
+++ PL_2017_10_24/ZAD_2_7.py
@@ -0,0 +1,28 @@
+# 2.7 Wczytaj zawarty w pliku json.
+# Stwórz plik który będzie posiadał wszystkie statki w zdaniach od najdroższego do najtańszego:
+# <Nazwa> kosztuje <123> credits.
+
+import json
+
+with open("py1.2zd.json","r") as file:
+    ship_data = json.load(file)
+
+unknown = []
+toSorted = []
+
+for element in ship_data:
+    if element['cost_in_credits'] == 'unknown':
+        slo = {}
+        slo = {'nazwa': element['starship_class'], 'cost': 'unknown'}
+        unknown.append(slo)
+    else:
+        slo = {}
+        slo = {'nazwa': element['starship_class'], 'cost': int(element['cost_in_credits'])}
+        toSorted.append(slo)
+
+sortedCrafts = sorted(toSorted, key=lambda ship : ship['cost'])
+
+allCrafts = sortedCrafts + unknown
+
+for element in allCrafts:
+    print('{0} kosztuje {1} credits.'.format(element['nazwa'],element['cost']))
